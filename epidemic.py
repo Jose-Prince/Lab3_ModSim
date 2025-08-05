231f21b5145b10ad3a6f31ee6a811a13a2981177
MarchMol <marchmol98@gmail.com>
2025-08-05 16:54:09 -0600

    feat: sir model with odeint implemented

--- epidemic.py
+++ epidemic.py
@@ -1,81 +1,69 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.integrate import odeint
 
 class EpidemicModel():
     def __init__(self, N, i0, g, b):
         
-        # Politics:
-            ## SIR no interventions
-        self.i0 = i0
+        # Initial conditions
         self.N = N
-        self.g = g
-        self.b = b
+        
+        self.s0 = N- i0    
+        self.i0 = i0
+        self.r0 = 0
         
         self.v = 0
+        
         self.case_threshold = i0+100
-        
-        self.s = N - i0
-        self.i = i0
-        self.r = 0
-        
-            
+        # Constants
+        self.g = g
+        self.b = b
+       
+        # Initial SIR in vector form
+        self.y0 = [self.s0, self.i0, self.r0]
         # Time
-        self.t = 0
-        self.dt = 1
+        self.t = None
         
-        # history
-        self.history = []
+        # Result
+        self.result = []
     
-    def get_v(self):
-        if self.i >= self.case_threshold:
+    def get_v(self, I, S):
+        if I >= self.case_threshold:
             self.v += 0.02
             self.case_threshold+=100
-        Rt = (self.b/self.g) * (self.s/self.N)
+        Rt = (self.b/self.g) * (S/self.N)
         if Rt>1:
             self.v*=1.5
-            
         return self.v
         
-    def step(self):
-        beta = self.b
-        gamma = self.g
-        v = self.get_v()
-        # Vaccination policy
-        # Flow
-        ds = (-(beta * self.s * self.i) - (v + self.s) )/ self.N
-        di = ((beta * self.s * self.i) - (gamma * self.i)) /self.N 
-        dr = ((gamma * self.i) + (v + self.s)) /self.N 
+    def SIR(self, y, t):
+        S, I, R = y
+        v = self.get_v(I, S)
+        beta, gamma = self.b, self.g
         
-        # Update Stocks
-        self.s+= ds*self.dt
-        self.i+= di*self.dt
-        self.r+= dr*self.dt
+        ds = -(beta * S * I)/ self.N - (v*S)
+        di = (beta*S*I)/self.N  - (gamma*I)
+        dr = (gamma * I)/self.N  + (v*S)
+        return [ds, di, dr]
         
-        # Time step
-        self.t+=self.dt
+    def sim(
+        self,
+        start,
+        stop,
+        num
+    ):
+        self.t = np.linspace(start, stop, num)
+        self.result = odeint(self.SIR, self.y0, self.t)
 
-        # Save
-        self.history.append([self.t, self.s, self.i, self.r])
-    
-    def sim(self, time_limit=None, sim_limit = None):
-        if time_limit!=None and time_limit>0:
-            while self.t <= time_limit:
-                self.step()
-
-        if sim_limit!= None:
-            for _ in range(sim_limit):
-                self.step()
 
     
     def plot(self):
-        history_dict = [{"t": h[0], "S": h[1], "I": h[2], "R": h[3]} for h in self.history]
-        df = pd.DataFrame(history_dict)
-        
+        S, I, R = self.result.T
         plt.title(f"Epidemia con Politicas")
-        plt.plot(df["t"], df["S"], label="Susceptibles")
-        plt.plot(df["t"], df["I"], label="Infectados")
-        plt.plot(df["t"], df["R"], label="Recuperados")
+        plt.plot(self.t, S, label="Susceptibles")
+        plt.plot(self.t, I, label="Infectados")
+        plt.plot(self.t, R, label="Recuperados")
         plt.xlabel("Dia")
         plt.ylabel("Poblacion")
         plt.tight_layout()
@@ -90,5 +78,5 @@ model = EpidemicModel(
     g=0.1
 )
 
-model.sim(time_limit=100) # 100 dias
+model.sim(0, 100, 500) # 100 dias
 model.plot()
